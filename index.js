4d98555c7ffe5400cd2e252b4c1982ab042737fa
Ville <ville.reima@gmail.com>
2023-08-10 00:59:37 +0300

    Ex 3.1 - 3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const people = {
   persons: [
     {
@@ -28,6 +30,51 @@ const people = {
 
 app.get("/api/persons", (req, res) => {
   res.json(people.persons);
+});
+
+app.post("/api/persons", (req, res) => {
+  const data = req.body;
+
+  console.log(data);
+
+  if (!data.name) {
+    return res.status(400).json({
+      error: "name missing",
+    });
+  }
+
+  if (!data.number) {
+    return res.status(400).json({
+      error: "number missing",
+    });
+  }
+
+  const id = Math.floor(Math.random() * 1000000);
+  const newPerson = {
+    name: data.name,
+    number: data.number,
+    id: id,
+  };
+
+  people.persons = people.persons.concat(newPerson);
+
+  return res.status(201).end();
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  people.persons = people.persons.filter((person) => person.id !== id);
+
+  res.status(204).end();
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = people.persons.find((person) => person.id === id);
+
+  if (!person) return res.status(404).end();
+
+  res.json(person);
 });
 
 app.get("/info", (req, res) => {
